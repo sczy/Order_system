865b68e0038b74847ad79bd332e4330e0fe2f576
sczy <778226391@qq.com>
2013-12-31 11:53:43 +0800

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OrderSystem::Application.config.secret_token = '6775a0d531a6e6612176c298316ac6f6bceaa72ff5e7c2843be1e6a601a9397011f192a36576a1b8c7d5ec8b087282c31a8467e49f9ba92c9e7f367c9497c7e7'
